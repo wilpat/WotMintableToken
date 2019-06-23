8f838b9016d1f74d55a10d3133f27cce84420e31
William Okafor <wilpat456@gmail.com>
2019-06-23 19:04:19 +0100

    Gives provision for other connections than metamask

--- docs/js/app.js
+++ docs/js/app.js
@@ -13,8 +13,15 @@ App = {
   },
 
   initWeb3: function() {
-    App.web3Provider = web3.currentProvider;
-    web3 = new Web3(web3.currentProvider);
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
     window.web3 = web3;
     return App.initContracts();
   },
